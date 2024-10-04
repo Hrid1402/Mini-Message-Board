db874625d4db2464205615e882b9a26d6f1703b9
Hrid1402 <Hrid1402@gmail.com>
2024-10-04 14:57:59 -0500

    feat: finished

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-console.log("Running");
+
+const express = require('express')
+const app = express()
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const messages = [
+  {
+    text: "Hi there!",
+    user: "(DEMO) Amando",
+    added: new Date().toLocaleString('en-GB', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    })
+  },
+  {
+    text: "Hello World!",
+    user: "(DEMO) Charles",
+    added: new Date().toLocaleString('en-GB', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    })
+  }
+];
 
-http.createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    let fileName =  "."+q.pathname;
-    fs.readFile(fileName == "./" ? "./index.html" : fileName, function(err, data) {
-        if (err) {
-            fs.readFile('404.html', function(err, data) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
-              });
-              return;
-          }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-      });
-}).listen(8080);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.post('/new', (request, response) => {
+  console.log(request.body);
+  messages.unshift({ user: request.body.messageAuthor, text: request.body.messageText, added: new Date().toLocaleString('en-GB', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  })});
+  response.redirect("/");
+});
+app.get('/', (request, response) => {
+  response.render("index", {title: "Mini Messageboard", messages: messages})
+  
+})
+app.get('/new', (request, response) => {
+  response.render("new", {});
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}/`)
+})
